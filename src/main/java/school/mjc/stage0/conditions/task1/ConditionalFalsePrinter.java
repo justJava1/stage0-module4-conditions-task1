0b19e7ffa9c1da6a0c4540a2311636398d62bdac
Kirill Gusakov <justmymail003@gmail.com>
2022-12-25 17:28:46 +0300

    a

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        boolean flag = false;
+        if(flag == false) {
+            System.out.println(flag);
+        } else {
+            System.out.println(flag);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        boolean flag = true;
+        if(flag == true) {
+            System.out.println(flag);
+        } else {
+            System.out.println(flag);
+        }
     }
 }
